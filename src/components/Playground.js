643b763f903efdc5d0b15c651697db0291418e0d
Satyam <nikamsatyam187@gmail.com>
2025-12-16 09:27:10 +0530

    Create Playground.js

---
+++ src/components/Playground.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Playground = () => {
+  return (
+    <div>
+      <h2>Playground</h2>
+      <p>Welcome to the Code Playground! This is a private route.</p>
+    </div>
+  );
+};
+
+export default Playground;
